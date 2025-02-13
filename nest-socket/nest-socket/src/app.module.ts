a5789bc116a90c75d9938f5a0828293c17ba52f8
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2025-02-13 11:21:47 +0000

    next issue: 환경변수 로드

--- nest-socket/nest-socket/src/app.module.ts
+++ nest-socket/nest-socket/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatGateway } from './app.gateway';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
 })
