e4b129cd8ff4dc2fa19236e0a23459af1d3344a7
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2025-02-17 04:59:46 +0000

    입장, 방 내에서 대화 나누기 구현 완료 but join버튼 클릭 이후 로직이 제대로 수행되지 않음

--- nest-socket/nest-socket/src/app.gateway.ts
+++ nest-socket/nest-socket/src/app.gateway.ts
@@ -19,6 +19,7 @@ export class ChatGateway {
 
 @WebSocketGateway({ namespace: 'room' })
 export class RoomGateway {
+  constructor(private readonly chatGateWay: ChatGateway) {}
   rooms: string[] = [];
 
   @WebSocketServer() server: Server;
@@ -26,7 +27,35 @@ export class RoomGateway {
   @SubscribeMessage('createRoom')
   handleMessage(@MessageBody() data) {
     const { nickname, room } = data;
+
+    this.chatGateWay.server.emit('notice', {
+      message: `${nickname}님이 그룹채팅방 <${room}>을 만들었습니다.`,
+    });
+
     this.rooms.push(room);
     this.server.emit('rooms', this.rooms);
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(socket: Socket, data) {
+    const { nickname, room, toLeaveRoom } = data;
+
+    socket.leave(toLeaveRoom);
+
+    this.chatGateWay.server.emit('notice', {
+      message: `${nickname}님이 그룹채팅방 <${room}>에 입장했습니다.`,
+    });
+
+    socket.join(room);
+  }
+
+  //메세지 전송하면 받아줄 핸들러
+  @SubscribeMessage('sendMessage')
+  handleSendMessageToRoom(socket: Socket, data) {
+    const { message, nickname, room } = data;
+    console.log(data);
+    socket.broadcast
+      .to(room)
+      .emit('sendMessage', { message: `${nickname}: ${message}` });
   }
 }
--- nest-socket/nest-socket/static/script.js
+++ nest-socket/nest-socket/static/script.js
@@ -1,17 +1,21 @@
-const socket = io(
-  'https://congenial-space-spork-p4pw5qj4qxgc7x4j-3000.app.github.dev/chat',
-); //chat 네임스페이스의 소켓 서버 주소
-const roomSocket = io(
-  'https://congenial-space-spork-p4pw5qj4qxgc7x4j-3000.app.github.dev/room',
-); //room 네임스페이스의 소켓 서버 주소
+//깃헙 올릴땐 codespace주소 지우고 올려라 제발
+const socket = io('codespace주소/chat'); //chat 네임스페이스의 소켓 서버 주소
+const roomSocket = io('codespace주소/room'); //room 네임스페이스의 소켓 서버 주소
 
 const nickname = prompt('사용하실 닉네임을 입력해주세요');
 let currentRoom = '';
 
 function sendMessage() {
+  if (currentRoom === '') {
+    alert('대화를 나눌 방을 선택해주세요');
+    return;
+  }
+
   const message = $('#message').val();
+  const data = { message, nickname, room: currentRoom };
+
   $('#chat').append(`<div>나: ${message}</div>`);
-  socket.emit('message', { message, nickname });
+  roomSocket.emit('sendMessage', data);
 }
 
 //채팅방을 생성하는 함수
@@ -27,7 +31,7 @@ roomSocket.on('rooms', (data) => {
 
   data.forEach((room) => {
     $('#rooms').append(
-      `<li>${room} <button onclick="joinRoom(${room})">join</button></li>`,
+      `<li>${room} <button onclick="joinRoom('${room}')">join</button></li>`,
     );
   });
 });
@@ -38,4 +42,20 @@ socket.on('connect', () => {
 
 socket.on('message', (message) => {
   $('#chat').append(`<div>${message}</div>`);
+});
+
+//공지영역 관련 이벤트 핸들러
+socket.on('notice', (data) => {
+  $('#notice').append(`<div>${data.message}</div>`);
+});
+
+//채팅방 입장 함수
+function joinRoom(room) {
+  roomSocket.emit('joinRoom', { room, nickname, toLeaveRoom: currentRoom });
+  currentRoom = room;
+}
+
+roomSocket.on('sendMessage', (data) => {
+  console.log(data);
+  $('#chat').append(`<div>${data.message}</div>`);
 });
